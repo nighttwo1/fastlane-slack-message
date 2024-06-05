10b41cebb3cfda50e8bf23a9394d762b3edd0338
nighttwo1 <ys12j1@gmail.com>
2024-06-06 00:55:26 +0900

    feat: fastlane action for sending slack message

---
+++ README.md
@@ -0,0 +1,98 @@
+# Fastlane Action for slack message
+
+Action for sending message with incoming webhook app in slack. Supports both simplified template and user customized block kit message.
+
+# Example Output
+
+This action will generate and send a message to Slack that looks like this:
+<img width="812" alt="스크린샷 2024-06-06 오전 12 43 04" src="https://github.com/nighttwo1/fastlane-slack-message/assets/43779571/52ca492f-8748-4cb8-a9bd-5b437c54d3b8">
+
+# Usage
+
+Here is an example of how to use this action in your `Fastfile`:
+
+### 1. Use predefiend template
+
+```ruby
+lane :post_to_slack do
+  slack_message(
+    title: "새로운 안드로이드 앱이 배포되었습니다! 🚀",
+    sub_title: "Checkout for new version of android app!",
+    webhook_url: "https://hooks.slack.com/services/...",
+    payload: {
+      "버전" => "0.1.10",
+      "패키지명" => "com.nighttwo1.slackmsg",
+      "Release Date" => "2024-06-05"
+    },
+    footer: "테스트 후 피드백은 언제든지 환영합니다. 앱 사용 중 문제가 발생하거나 개선 사항이 있다면 메시지를 남겨주시면 감사하겠습니다.",
+  )
+end
+```
+
+### 2. Use customed block kit message
+
+```ruby
+lane :post_to_slack_with_custom_body do
+  slack_message(
+    webhook_url: "https://hooks.slack.com/services/...",
+    custom_body: '{
+      "blocks": [
+          {
+            "type": "header",
+            "text": {
+              "type": "plain_text",
+              "text": "새로운 안드로이드 앱이 배포되었습니다! 🚀",
+              "emoji": true
+            }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "plain_text",
+              "text": "Checkout for new version of android app!",
+              "emoji": true
+            }
+          },
+          {
+            "type": "section",
+            "fields": [
+              {
+                "type": "mrkdwn",
+                "text": "*버전:* `0.1.10`"
+              },
+              {
+                "type": "mrkdwn",
+                "text": "*패키지명:* `com.nighttwo1.slackmsg`"
+              },
+              {
+                "type": "mrkdwn",
+                "text": "*Release Date:* `2024-06-05`"
+              }
+            ]
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "테스트 후 피드백은 언제든지 환영합니다. 앱 사용 중 문제가 발생하거나 개선 사항이 있다면 메시지를 남겨주시면 감사하겠습니다."
+            }
+          }
+        ]
+      }'
+  )
+end
+```
+
+# Parameters
+
+| Key           | Description                                                                                                              | Environment Variable           | Default         | Type   | Optional |
+| ------------- | ------------------------------------------------------------------------------------------------------------------------ | ------------------------------ | --------------- | ------ | -------- |
+| `title`       | The title displayed on the Slack message.                                                                                | `FL_SLACK_MESSAGE_TITLE`       | `새로운 메시지` | String | No       |
+| `sub_title`   | The subtitle displayed on the Slack message.                                                                             | `FL_SLACK_MESSAGE_SUB_TITLE`   |                 | String | Yes      |
+| `webhook_url` | The Incoming WebHook URL for your Slack group.                                                                           | `FL_SLACK_MESSAGE_WEBHOOK_URL` |                 | String | No       |
+| `payload`     | A hash containing additional information to be displayed in the message.                                                 | `FL_SLACK_MESSAGE_PAYLOAD`     | `{}`            | Hash   | No       |
+| `footer`      | Additional message displayed at the bottom of the Slack message.                                                         | `FL_SLACK_MESSAGE_FOOTER`      |                 | String | Yes      |
+| `custom_body` | A complete custom body for the Slack message in JSON format. If this is provided, it will override the other parameters. | `FL_SLACK_MESSAGE_CUSTOM_BODY` |                 | String | Yes      |
---
+++ fastlane/actions/slack_message.rb
@@ -0,0 +1,181 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      SLACK_MESSAGE_CUSTOM_VALUE = :SLACK_MESSAGE_CUSTOM_VALUE
+    end
+
+    require 'uri'
+    require 'net/http'
+
+    class SlackMessageAction < Action
+      def self.run(params)
+        webhook_url = params[:webhook_url]
+        payload_json = ""
+        if params[:custom_body]
+          payload_json = params[:custom_body]
+        else
+          payload_json = self.generate_slack_message(params)
+        end
+
+        uri = URI.parse(webhook_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' => 'application/json'})
+        request.body = payload_json
+
+        response = http.request(request)
+        if response.code.to_i == 200
+          UI.message("Successfully sent message to Slack")
+        else
+          UI.error("Failed to send message to Slack: #{response.body}")
+          raise FastlaneException.new(response.body)
+        end
+        
+        
+        # fastlane will take care of reading in the parameter and fetching the environment variable:
+        # UI.message("Parameter API Token: #{params[:api_token]}")
+
+        # sh "shellcommand ./path"
+
+        # Actions.lane_context[SharedValues::SLACK_MESSAGE_CUSTOM_VALUE] = "my_val"
+      end
+
+      def self.generate_slack_message(options)
+        header = {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: options[:title],
+            emoji: true
+          }
+        }
+
+        sub_title = {
+          type: "section",
+          text: {
+            type: "plain_text",
+            text: options[:sub_title],
+            emoji: true
+          }
+        }
+
+        content = {
+          type: "section",
+          fields: []
+        }
+        content[:fields] = options[:payload].map do |key, value|
+          {
+            type: "mrkdwn",
+            text: "*#{key.to_s}:* `#{value.to_s}`"
+          }
+        end
+
+
+        divider = {
+          type: "divider"
+        }
+
+        footer = {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: options[:footer]
+          }
+        }
+
+        post_message = {
+          blocks: []
+        }
+        post_message[:blocks].push(header)
+        post_message[:blocks].push(sub_title)
+        post_message[:blocks].push(content)
+        post_message[:blocks].push(divider)
+        post_message[:blocks].push(footer)
+
+        post_message.to_json
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        'A short description with <= 80 characters of what this action does'
+      end
+
+      def self.details
+        # Optional:
+        # this is your chance to provide a more detailed description of this action
+        'You can use this action to do cool things...'
+      end
+
+      def self.available_options
+        # Define all options your action supports.
+
+        # Below a few examples
+        [
+          FastlaneCore::ConfigItem.new(key: :title,
+                                       # The name of the environment variable
+                                       env_name: 'FL_SLACK_MESSAGE_TITLE',
+                                       # a short description of this parameter
+                                       description: 'Title displayed on Slack message',
+                                       default_value: "새로운 메시지"),
+          FastlaneCore::ConfigItem.new(key: :sub_title,
+                                       # The name of the environment variable
+                                       env_name: 'FL_SLACK_MESSAGE_SUB_TITLE',
+                                       # a short description of this parameter
+                                       description: 'Sub title displayed on Slack message',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :webhook_url,
+                                       # The name of the environment variable
+                                       env_name: 'FL_SLACK_MESSAGE_WEBHOOK_URL',
+                                       # a short description of this parameter
+                                       description: 'Incoming WebHook for your Slack group',
+                                       verify_block: proc do |value|
+                                        UI.user_error!("Invalid URL, must start with https://") unless value.start_with?("https://")
+                                      end),
+          FastlaneCore::ConfigItem.new(key: :payload,
+                                       # The name of the environment variable
+                                       env_name: 'FL_SLACK_MESSAGE_PAYLOAD',
+                                       # a short description of this parameter
+                                       description: 'Add additional information to this post. payload must be a hash containing any key with any value',
+                                       default_value: {},
+                                       type: Hash),
+          FastlaneCore::ConfigItem.new(key: :footer,
+                                       # The name of the environment variable
+                                       env_name: 'FL_SLACK_MESSAGE_FOOTER',
+                                       # a short description of this parameter
+                                       description: 'Additional message',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :custom_body,
+                                       # The name of the environment variable
+                                       env_name: 'FL_SLACK_MESSAGE_CUSTOM_BODY',
+                                       # a short description of this parameter
+                                       description: 'Additional message',
+                                       optional: true),
+        ]
+      end
+
+      def self.output
+        # Define the shared values you are going to provide
+        # Example
+        [
+          ['SLACK_MESSAGE_CUSTOM_VALUE', 'A description of what this value contains']
+        ]
+      end
+
+      def self.return_value
+        # If your method provides a return value, you can describe here what it does
+      end
+
+      def self.authors
+        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
+        ['Your GitHub/Twitter Name']
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
